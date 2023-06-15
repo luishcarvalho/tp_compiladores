8f4a897418cc8bc69dd5c3fdeda02b18c564232a
luishcarvalho <luis.h.carvalho@ufv.br>
2023-06-14 23:42:30 -0300

    tentando verifica conflito

--- ListaEncadeada/lista.c
+++ ListaEncadeada/lista.c
@@ -28,13 +28,24 @@ void insertCell(LinkedList *list, char *symbol, char *symbolType, char *type, in
         cell->pProx = NULL;
         list->end->pProx = cell;
         list->end = cell;
-    }
+    }}
 
-    /*
-    list->end->pProx = (Cell*)malloc(CELL_SIZE); //TEM QUE VER SE FUNCIONA
-    list->end = list->end->pProx;
-    list->end->input = newInput;
-    list->end->pProx = NULL;
-    */
+
+
+int VerificaConflito(Input input, LinkedList *list)
+{
+    Cell* ponteiroauxiliar;
+    ponteiroauxiliar = list->start;
+    // 0 = false / 1 = true
+    int digitoverificador = 0;
+    //varre a lista
+    while (ponteiroauxiliar != NULL){
+        //verifica se o simbolo passado bate com algum da lista
+        if (getSymbol(ponteiroauxiliar->input) == getSymbol(input)){
+            digitoverificador = 1;
+            break;}
+        ponteiroauxiliar = ponteiroauxiliar->pProx;}
+    
+    return digitoverificador;
 }
 
